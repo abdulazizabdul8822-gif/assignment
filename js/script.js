4ca337f6e820c1d8398c6a29858a4d58079c80e0
Md Azizul Islam <mdazizulislam@Mds-MacBook-Air.local>
2026-03-07 00:22:35 +0600

    If I enter the correct username and password, it successfully takes me to the homepage. This function is done in JS.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("hell assignment")
+document.getElementById("loginBtn").addEventListener("click", function(){
+    const inputUsername = document.getElementById("inputUsername");
+    const userName = inputUsername.value;
+    console.log(userName);
+
+    const inputPin = document.getElementById("inputPin");
+    const pin = inputPin.value;
+    console.log(pin);
+
+
+    if(userName == "admin" && pin == "123"){
+        alert("Login Success");
+        window.location.assign("/home.html");
+    } else{
+        alert("Login Failed");
+        return;
+    }
+});
