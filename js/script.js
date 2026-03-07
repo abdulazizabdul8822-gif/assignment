150fc5e937e8b5c6f52bda6add7753a8caf97607
Md Azizul Islam <mdazizulislam@Mds-MacBook-Air.local>
2026-03-07 12:55:56 +0600

    Html design allbar and issue bar complete

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
-document.getElementById("loginBtn").addEventListener("click", function(){
-    const inputUsername = document.getElementById("inputUsername");
-    const userName = inputUsername.value;
-    console.log(userName);
 
-    const inputPin = document.getElementById("inputPin");
-    const pin = inputPin.value;
-    console.log(pin);
+const loginBtn = document.getElementById("loginBtn");
 
 
-    if(userName == "admin" && pin == "123"){
-        alert("Login Success");
-        window.location.assign("/home.html");
-    } else{
-        alert("Login Failed");
-        return;
-    }
-});
+if (loginBtn) {
+    loginBtn.addEventListener("click", function () {
+        const userName = document.getElementById("inputUsername").value;
+        const pin = document.getElementById("inputPin").value;
+
+        if (userName === "admin" && pin === "123") {
+            alert("Login Success");
+            window.location.href = "home.html";
+        } else {
+            alert("Login Failed");
+        }
+    });
+}
